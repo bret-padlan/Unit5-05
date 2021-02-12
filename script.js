68724c7f03338facc544e74de1668cb23e19811b
bretpadlan <bret.padlan@ocsbstudent.ca>
2021-02-12 18:19:36 +0000

    script.js - remove spacings

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 let number = 0
-let sentence = 'hello, this is number 4.'
 
 document.getElementById('button').addEventListener('click', calculatingButton)
 
 function calculatingButton () {
   number = document.getElementById('table').value
-
   number = parseInt(number)
-
   number = number * 4
-  
   alert(number)
 }
